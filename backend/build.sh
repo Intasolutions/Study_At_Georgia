4b727ab05d57ba546621a06d859c782c95525527
Akash <intasolutionpvtltd@gmail.com>
2026-08-20 23:03:25 +0530

    vgxvsh

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Fake the 0007 migration to bypass the DuplicateTable error on Render
+python manage.py migrate api 0007_teammember_virtualtour --fake
+
 python manage.py migrate
